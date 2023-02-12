16a0e7d59d8ad2b8f57ac5d9842c597b8a1c50a9
xiaozhi <1477818996@qq.com>
2023-02-12 23:36:55 +0800

    ADD ArticleAppService

---
+++ src/SherCore.BlogServer.Application.Contracts/Articles/IArticleAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Services;
+
+namespace SherCore.BlogServer.Articles
+{
+    public interface IArticleAppService:ICrudAppService< //Defines CRUD methods
+            ArticleDto, //Used to show Entity
+            Guid, //Primary key of the  entity
+            ArticleRequestDto, //Used for paging/sorting
+            ArticleCreateOrUpdateDto> //Used to create/update a entity
+    {
+
+    }
+}
---
+++ src/SherCore.BlogServer.Application/Articles/ArticleAppService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace SherCore.BlogServer.Articles
+{
+    public class ArticleAppService : BlogServerAppService, IArticleAppService
+    {
+        private readonly IRepository<Article, Guid> _articleReository;
+        public ArticleAppService(IRepository<Article,Guid> baseRepo)
+        {
+            _articleReository = baseRepo;
+        }
+        public async Task<ArticleDto> CreateAsync(ArticleCreateOrUpdateDto input)
+        {
+            var entity = ObjectMapper.Map<ArticleCreateOrUpdateDto, Article>(input);
+
+            await _articleReository.InsertAsync(entity);
+
+            return ObjectMapper.Map<Article, ArticleDto>(entity);
+
+         }
+
+        public async Task DeleteAsync(Guid id)
+        {
+           await _articleReository.DeleteAsync(id);
+        }
+
+        public  Task<ArticleDto> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResultDto<ArticleDto>> GetListAsync(ArticleRequestDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ArticleDto> UpdateAsync(Guid id, ArticleCreateOrUpdateDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        ///  查询条件构造IQuery
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private async Task<IQueryable<Article>> BulidFiledQuery(ArticleRequestDto input) 
+        {
+            var query =await _articleReository.GetQueryableAsync();
+
+            query = query
+                   .WhereIf(input.Title.IsNullOrEmpty(),x=>x.Titile.Contains(input.Title));///模糊查询
+
+            return query;
+        }
+    }
+}
