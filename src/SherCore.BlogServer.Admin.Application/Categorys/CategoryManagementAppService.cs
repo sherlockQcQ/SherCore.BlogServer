058a30e04f100eb0badcea5d40b7273e33478b2f
小枝 <1477818996@qq.com>
2023-04-27 14:23:29 +0800

    update

--- src/SherCore.BlogServer.Admin.Application/Categorys/CategoryManagementAppService.cs
+++ src/SherCore.BlogServer.Admin.Application/Categorys/CategoryManagementAppService.cs
@@ -1,99 +1,97 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using SherCore.BlogServer.Categorys;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using SherCore.BlogServer.Categorys;
 using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 
-namespace SherCore.BlogServer.Admin.Categorys
+namespace SherCore.BlogServer.Admin.Categorys;
+
+[Authorize]
+[RemoteService(IsEnabled = false)]
+public class CategoryManagementAppService : AdminAppService, ICategoryManagementAppService
 {
-    [Authorize()]
-    [RemoteService(IsEnabled = false)]
-    public class CategoryManagementAppService : AdminAppService, ICategoryManagementAppService
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryManagementAppService(
+        ICategoryRepository categoryRepository)
     {
-        private readonly ICategoryRepository _categoryRepository;
+        _categoryRepository = categoryRepository;
+    }
 
-        public CategoryManagementAppService(
-            ICategoryRepository categoryRepository)
+    public async Task<CategoryDto> CreateAsync(CreateCategoryDto input)
+    {
+        var category = new Category(
+            GuidGenerator.Create(),
+            input.Name,
+            input.IsEnable,
+            input.Sort);
+
+        await _categoryRepository.InsertAsync(category);
+
+        return ObjectMapper.Map<Category, CategoryDto>(category);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _categoryRepository.DeleteAsync(id);
+    }
+
+    public async Task DeleteManyAsync(List<Guid> ids)
+    {
+        await _categoryRepository.DeleteManyAsync(ids);
+    }
+
+    public async Task<CategoryDto> GetAsync(Guid id)
+    {
+        var category = await _categoryRepository.FindAsync(id);
+
+        return ObjectMapper.Map<Category, CategoryDto>(category);
+    }
+
+    public async Task<PagedResultDto<CategoryDto>> GetListAsync(CategoryQueryOption input)
+    {
+        var query = await _categoryRepository
+            .BuildFieldQuery(input.Name);
+
+        var items = await AsyncExecuter.ToListAsync(query.PageBy(input));
+        var count = await AsyncExecuter.CountAsync(query);
+
+        var dtoList = new List<CategoryDto>(ObjectMapper.Map<List<Category>, List<CategoryDto>>(items));
+
+        return new PagedResultDto<CategoryDto>
         {
-            _categoryRepository = categoryRepository;
-        }
+            Items = dtoList,
+            TotalCount = count
+        };
+    }
 
-        public async Task<CategoryDto> CreateAsync(CreateCategoryDto input)
-        {
-            var category = new Category(
-                id: GuidGenerator.Create(),
-                name: input.Name,
-                isEnable: input.IsEnable,
-                sort: input.Sort);
+    public async Task<CategoryDto> UpdateAsync(Guid id, UpdateCategoryDto input)
+    {
+        var entity = await _categoryRepository.FindAsync(id);
 
-            await _categoryRepository.InsertAsync(category);
+        entity.Sort = input.Sort;
+        entity.IsEnable = input.IsEnable;
+        entity.Name = input.Name;
 
-            return ObjectMapper.Map<Category, CategoryDto>(category);
-        }
+        await _categoryRepository.UpdateAsync(entity);
 
-        public async Task DeleteAsync(Guid id)
-        {
-            await _categoryRepository.DeleteAsync(id);
-        }
+        return await GetAsync(id);
+    }
 
-        public async Task DeleteManyAsync(List<Guid> ids)
-        {
-            await _categoryRepository.DeleteManyAsync(ids);
-        }
+    /// <summary>
+    ///     获取专栏数据（key -value ）
+    /// </summary>
+    /// <returns></returns>
+    public async Task<List<CategorySelectDto>> GetSelectListAsync()
+    {
+        var query = await _categoryRepository.GetQueryableAsync();
 
-        public async Task<CategoryDto> GetAsync(Guid id)
-        {
-            var category = await _categoryRepository.FindAsync(id);
+        var selectList = await AsyncExecuter
+            .ToListAsync(query.Select(x => new CategorySelectDto { Key = x.Id, Value = x.Name }));
 
-            return ObjectMapper.Map<Category, CategoryDto>(category);
-        }
-
-        public async Task<PagedResultDto<CategoryDto>> GetListAsync(CategoryQueryOption input)
-        {
-            var query = await _categoryRepository
-                    .BuildFieldQuery(input.Name);
-
-            var items = await AsyncExecuter.ToListAsync(query.PageBy(input));
-            var count = await AsyncExecuter.CountAsync(query);
-
-            var dtos = new List<CategoryDto>(ObjectMapper.Map<List<Category>, List<CategoryDto>>(items));
-
-            return new PagedResultDto<CategoryDto>()
-            {
-                Items = dtos,
-                TotalCount = count
-            };
-        }
-
-        public async Task<CategoryDto> UpdateAsync(Guid id, UpdateCategoryDto input)
-        {
-            var entity = await _categoryRepository.FindAsync(id);
-
-            entity.Sort = input.Sort;
-            entity.IsEnable = input.IsEnable;
-            entity.Name = input.Name;
-
-            await _categoryRepository.UpdateAsync(entity);
-
-            return await GetAsync(id);
-        }
-
-        /// <summary>
-        /// 获取专栏数据（key -value ）
-        /// </summary>
-        /// <returns></returns>
-
-        public async Task<List<CategorySelectDto>> GetSelectListAsync()
-        {
-            var query = await _categoryRepository.GetQueryableAsync();
-
-            var selectList = await AsyncExecuter
-                    .ToListAsync(query.Select(x => new CategorySelectDto() { Key = x.Id, Value = x.Name }));
-
-            return selectList;
-        }
+        return selectList;
     }
 }
--- src/SherCore.BlogServer.Admin.Application/Posts/PostManagementAppService.cs
+++ src/SherCore.BlogServer.Admin.Application/Posts/PostManagementAppService.cs
@@ -13,122 +13,121 @@ using Volo.Abp.Application.Dtos;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
 
-namespace SherCore.BlogServer.Admin.Posts
+namespace SherCore.BlogServer.Admin.Posts;
+
+[Authorize]
+[RemoteService(IsEnabled = false)]
+public class PostManagementAppService : AdminAppService, IPostManagementAppService
 {
-    [Authorize()]
-    [RemoteService(IsEnabled = false)]
-    public class PostManagementAppService : AdminAppService, IPostManagementAppService
+    private readonly CategoryManager _categoryManager;
+    private readonly PostManager _postManager;
+    private readonly IPostRepository _postRepository;
+    private readonly ITagRepository _tagRepository;
+    private readonly IRepository<IdentityUser, Guid> _userRepository;
+
+    public PostManagementAppService(
+        IPostRepository postRepository,
+        ITagRepository tagRepository,
+        CategoryManager categoryManager,
+        IRepository<IdentityUser, Guid> userRepository,
+        PostManager postManager)
     {
-        private readonly IPostRepository _postRepository;
-        private readonly ITagRepository _tagRepository;
-        private readonly IRepository<IdentityUser, Guid> _userRepository;
-        private readonly PostManager _postManager;
-        private readonly CategoryManager _categoryManager;
+        _postRepository = postRepository;
+        _tagRepository = tagRepository;
+        _categoryManager = categoryManager;
+        _userRepository = userRepository;
+        _postManager = postManager;
+    }
 
-        public PostManagementAppService(
-            IPostRepository postRepository,
-            ITagRepository tagRepository,
-            CategoryManager categoryManager,
-            IRepository<IdentityUser, Guid> userRepository,
-            PostManager postManager)
+    public async Task<PostWithDetailsDto> CreateAsync(CreatePostDto input)
+    {
+        var newPost = new Post(GuidGenerator.Create(), input.Title, input.IsReprint, input.CategoryId)
         {
-            _postRepository = postRepository;
-            _tagRepository = tagRepository;
-            _categoryManager = categoryManager;
-            _userRepository = userRepository;
-            _postManager = postManager;
+            Content = input.Content,
+            PublishDateTime = input.IsTiming ? input.PublishDateTime : DateTime.Now,
+            IsTop = input.IsTop
+        };
+
+        if (input.IsTiming)
+        {
+            newPost.Status = EnumStatus.Future;
+            /*   todo?
+             *   1.创建发布任务。
+             *   2.邮件通知订阅。
+             */
         }
 
-        public async Task<PostWithDetailsDto> CreateAsync(CreatePostDto input)
+        newPost.SetDescription();
+        var tagList = input.Tags.SplitByComma();
+        await _postManager.SaveTags(tagList, newPost);
+
+        return ObjectMapper.Map<Post, PostWithDetailsDto>(await _postManager.CreateAsync(newPost));
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _postManager.DeleteAsync(id);
+    }
+
+    public async Task DeleteManyAsync(List<Guid> ids)
+    {
+        if (!ids.Any())
         {
-            var newPost = new Post(GuidGenerator.Create(), input.Title, input.IsReprint, input.CategoryId)
-            {
-                Content = input.Content,
-                PublishDateTime = input.IsTiming ? input.PublishDateTime : DateTime.Now,
-                IsTop = input.IsTop,
-            };
-
-            if (input.IsTiming)
-            {
-                newPost.Status = EnumStatus.Future;
-                /*   todo?
-                 *   1.创建发布任务。
-                 *   2.邮件通知订阅。
-                 */
-            }
-
-            newPost.SetDescription();
-            var tagList = input.Tags.SplitByComma();
-            await _postManager.SaveTags(tagList, newPost);
-
-            return ObjectMapper.Map<Post, PostWithDetailsDto>(await _postManager.CreateAsync(newPost));
+            throw new UserFriendlyException("选择的文章未找到！未删除文章！");
         }
 
-        public async Task DeleteAsync(Guid id)
+        await _postManager.DeleteManyByIds(ids);
+    }
+
+    public async Task<PostWithDetailsDto> GetAsync(Guid id)
+    {
+        var post = await _postRepository.GetAsync(id);
+
+        var dto = ObjectMapper.Map<Post, PostWithDetailsDto>(post);
+
+        dto.Tags = ObjectMapper.Map<List<Tag>, List<TagDto>>(await _postManager.GetTagsOfPostAsync(id));
+
+        return dto;
+    }
+
+    public async Task<PagedResultDto<PostWithDetailsDto>> GetListAsync(PostQueryOptionDto input)
+    {
+        var query = await _postManager.BuildIQueryable(
+            ObjectMapper.Map<PostQueryOptionDto, PostQueryOption>(input));
+
+        var count = await AsyncExecuter.CountAsync(query);
+        var items = await AsyncExecuter
+            .ToListAsync(query.PageBy(input).OrderBy(input.Sorting ?? "CreationTime Desc"));
+
+        var dtoList = ObjectMapper.Map<List<Post>, List<PostWithDetailsDto>>(items);
+
+        var names = await _categoryManager.LookupNameByIdAsync(dtoList.Select(x => x.CategoryId).Distinct()
+            .ToArray());
+        var userIds = dtoList.Select(x => x.CreatorId).Distinct().ToList();
+        var users = await _userRepository.GetListAsync(x => userIds.Contains(x.Id)); // todo？扩展后需要重写
+
+        dtoList.ForEach(dto =>
         {
-            await _postManager.DeleteAsync(id);
-        }
+            //如果删除分类时不删除文章// todo？扩展后需要重写
+            dto.CategoryName = names.TryGetValue(dto.CategoryId, out var value) ? value : "已删除的分类专栏";
+            dto.UserName = users.FirstOrDefault(x => x.Id == dto.CreatorId)?.UserName;
+        });
 
-        public async Task DeleteManyAsync(List<Guid> ids)
+        return new PagedResultDto<PostWithDetailsDto>
         {
-            if (!ids.Any())
-            {
-                throw new UserFriendlyException("选择的文章未找到！未删除文章！");
-            }
+            Items = dtoList,
+            TotalCount = count
+        };
+    }
 
-            await _postManager.DeleteManyByIds(ids);
-        }
+    public async Task<PostWithDetailsDto> UpdateAsync(Guid id, UpdatePostDto input)
+    {
+        var entity = await _postRepository.FindAsync(id);
 
-        public async Task<PostWithDetailsDto> GetAsync(Guid id)
-        {
-            var post = await _postRepository.GetAsync(id);
+        ObjectMapper.Map(entity, input);
 
-            var dto = ObjectMapper.Map<Post, PostWithDetailsDto>(post);
+        await _postRepository.UpdateAsync(entity);
 
-            dto.Tags = ObjectMapper.Map<List<Tag>, List<TagDto>>(await _postManager.GetTagsOfPostAsync(id));
-
-            return dto;
-        }
-
-        public async Task<PagedResultDto<PostWithDetailsDto>> GetListAsync(PostQueryOptionDto input)
-        {
-            var query = await _postManager.BuildIQueryable(
-                ObjectMapper.Map<PostQueryOptionDto, PostQueryOption>(input));
-
-            var count = await AsyncExecuter.CountAsync(query);
-            var items = await AsyncExecuter
-                .ToListAsync(query.PageBy(input).OrderBy(input.Sorting ?? "CreationTime Desc"));
-
-            var dtoList = ObjectMapper.Map<List<Post>, List<PostWithDetailsDto>>(items);
-
-            var names = await _categoryManager.LookupNameByIdAsync(dtoList.Select(x => x.CategoryId).Distinct()
-                .ToArray());
-            var userIds = dtoList.Select(x => x.CreatorId).Distinct().ToList();
-            var users = await _userRepository.GetListAsync(x => userIds.Contains(x.Id)); // todo？扩展后需要重写
-
-            dtoList.ForEach(dto =>
-            {
-                //如果删除分类时不删除文章// todo？扩展后需要重写
-                dto.CategoryName = names.TryGetValue(dto.CategoryId, out string value) ? value : "已删除的分类专栏";
-                dto.UserName = users.FirstOrDefault(x => x.Id == dto.CreatorId)?.UserName;
-            });
-
-            return new PagedResultDto<PostWithDetailsDto>
-            {
-                Items = dtoList,
-                TotalCount = count
-            };
-        }
-
-        public async Task<PostWithDetailsDto> UpdateAsync(Guid id, UpdatePostDto input)
-        {
-            var entity = await _postRepository.FindAsync(id);
-
-            ObjectMapper.Map(entity, input);
-
-            await _postRepository.UpdateAsync(entity);
-
-            return ObjectMapper.Map<Post, PostWithDetailsDto>(entity);
-        }
+        return ObjectMapper.Map<Post, PostWithDetailsDto>(entity);
     }
 }
